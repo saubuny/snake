b0dfef904a4939e295bc445eb2e32c41e337d2b4
saubuny <122068609+saubuny@users.noreply.github.com>
2023-04-24 12:50:37 -0400

    black clear color, begin work on grid

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use bevy::{prelude::*, sprite::MaterialMesh2dBundle, window::PrimaryWindow};
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
+        // .add_startup_system(draw_grid)
+        // .add_system(player_movement)
         .run();
 }
 
@@ -16,14 +19,18 @@ pub struct Player {}
 #[derive(Component)]
 pub struct Food {}
 
-pub fn spawn_player(
+#[derive(Component)]
+pub struct Grid {}
+
+// Grid for easy viewing of tiles
+pub fn draw_grid(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let window = window_query.get_single().unwrap();
-    let init_pos = Vec3::new(window.height() / TILE_SIZE, window.width() / TILE_SIZE, 0.0);
+    let init_pos = Vec3::new(window.height() / 2.0, window.width() / 2.0, 0.0);
 
     commands.spawn((
         MaterialMesh2dBundle {
@@ -34,10 +41,34 @@ pub fn spawn_player(
             transform: Transform::from_translation(init_pos),
             ..default()
         },
+        Grid {},
+    ));
+}
+
+// Our snake :D
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let window = window_query.get_single().unwrap();
+    let init_pos = Vec3::new(window.height() / 2.0, window.width() / 2.0, 0.0);
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::Quad::new(Vec2::new(TILE_SIZE, TILE_SIZE)).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(init_pos),
+            ..default()
+        },
         Player {},
     ));
 }
 
+// Basic 2D Camera
 pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
 
@@ -45,4 +76,33 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+// Grid movement
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let Ok(mut transform) = player_query.get_single_mut() else {
+        return;
+    };
+
+    let mut direction = Vec3::ZERO;
+
+    if keyboard_input.pressed(KeyCode::W) {
+        direction += Vec3::new(0.0, 1.0, 0.0);
+    } else if keyboard_input.pressed(KeyCode::A) {
+        direction += Vec3::new(-1.0, 0.0, 0.0);
+    } else if keyboard_input.pressed(KeyCode::S) {
+        direction += Vec3::new(0.0, -1.0, 0.0);
+    } else if keyboard_input.pressed(KeyCode::D) {
+        direction += Vec3::new(1.0, 0.0, 0.0);
+    }
+
+    if direction.length() > 0.0 {
+        direction = direction.normalize();
+    }
+
+    transform.translation += direction * TILE_SIZE * time.delta_seconds();
 }
