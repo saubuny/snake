07580e56df66c420afb4d52931b54767953743f3
saubuny <122068609+saubuny@users.noreply.github.com>
2023-04-25 11:23:27 -0400

    fail to implement grids on own, switching to a tutorial to understand more about bevy

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
-        .add_startup_system(spawn_player)
         .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_snake_head)
         // .add_startup_system(draw_grid)
-        // .add_system(player_movement)
         .run();
 }
 
 const TILE_SIZE: f32 = 64.0;
 
 #[derive(Component)]
-pub struct Player {}
+pub struct SnakeHead {}
 
 #[derive(Component)]
 pub struct Food {}
@@ -30,30 +29,32 @@ pub fn draw_grid(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let window = window_query.get_single().unwrap();
-    let init_pos = Vec3::new(window.height() / 2.0, window.width() / 2.0, 0.0);
 
-    commands.spawn((
-        MaterialMesh2dBundle {
-            mesh: meshes
-                .add(shape::Box::new(TILE_SIZE, TILE_SIZE, 1.0).into())
-                .into(),
-            material: materials.add(ColorMaterial::from(Color::PURPLE)),
-            transform: Transform::from_translation(init_pos),
-            ..default()
-        },
-        Grid {},
-    ));
+    // Painful amount of casting
+    for x in (0..window.width() as i32).step_by(TILE_SIZE as usize) {
+        for y in (0..window.height() as i32).step_by(TILE_SIZE as usize) {
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(shape::Quad::new(Vec2::new(TILE_SIZE, TILE_SIZE)).into())
+                        .into(),
+                    material: materials.add(ColorMaterial::from(Color::WHITE)),
+                    transform: Transform::from_xyz(x as f32, y as f32, 1.0),
+                    ..default()
+                },
+                Grid {},
+            ));
+        }
+    }
 }
 
-// Our snake :D
-pub fn spawn_player(
+pub fn spawn_snake_head(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let window = window_query.get_single().unwrap();
-    let init_pos = Vec3::new(window.height() / 2.0, window.width() / 2.0, 0.0);
 
     commands.spawn((
         MaterialMesh2dBundle {
@@ -61,10 +62,14 @@ pub fn spawn_player(
                 .add(shape::Quad::new(Vec2::new(TILE_SIZE, TILE_SIZE)).into())
                 .into(),
             material: materials.add(ColorMaterial::from(Color::PURPLE)),
-            transform: Transform::from_translation(init_pos),
+            transform: Transform::from_translation(Vec3::new(
+                window.height() / 2.0,
+                window.width() / 2.0,
+                1.0,
+            )),
             ..default()
         },
-        Player {},
+        SnakeHead {},
     ));
 }
 
@@ -76,33 +81,4 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
-}
-
-// Grid movement
-pub fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<&mut Transform, With<Player>>,
-    time: Res<Time>,
-) {
-    let Ok(mut transform) = player_query.get_single_mut() else {
-        return;
-    };
-
-    let mut direction = Vec3::ZERO;
-
-    if keyboard_input.pressed(KeyCode::W) {
-        direction += Vec3::new(0.0, 1.0, 0.0);
-    } else if keyboard_input.pressed(KeyCode::A) {
-        direction += Vec3::new(-1.0, 0.0, 0.0);
-    } else if keyboard_input.pressed(KeyCode::S) {
-        direction += Vec3::new(0.0, -1.0, 0.0);
-    } else if keyboard_input.pressed(KeyCode::D) {
-        direction += Vec3::new(1.0, 0.0, 0.0);
-    }
-
-    if direction.length() > 0.0 {
-        direction = direction.normalize();
-    }
-
-    transform.translation += direction * TILE_SIZE * time.delta_seconds();
 }
